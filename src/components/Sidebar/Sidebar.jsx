4f48bb01661b2195d7db4bd9d486c4edc9474b6b
SAjay02 <56510.ajay@gmail.com>
2025-05-24 14:50:25 +0530

    fix: side bar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import "../../styles/Sidebar/Sidebar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDashboard,faHistory,faCalendar,faSatelliteDish,faExpandAlt,faPerson,faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faDashboard,faHistory,faCalendar,faChartSimple,faExpandAlt,faPerson,faPhone,faGear } from '@fortawesome/free-solid-svg-icons'
 const Sidebar = () => {
   return (
-    // <div>
+    <div>
         <div class="container1">
         <div>
             <h4 class="general">General</h4>
-            <h6 class="project" style={{color:"#051a78"}}><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faDashboard} /></span>Dashboard</h6>
-            <h6 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faHistory} /></span>History</h6>
-            <h6 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faCalendar} /></span>Calendar</h6>
-            <h6 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faExpandAlt} /></span>Appointments</h6>
-            <h6 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faSatelliteDish} /></span>Statistics</h6>
-        {/* </div>
-        <div> */}
-            <h4 class="general">Tools</h4>
-            <h6 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faPerson} /></span>Chat</h6>
-            <h6 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faPhone} /></span>Support</h6>
+            <h5 class="project" style={{color:"#051a78"}}><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faDashboard} /></span>Dashboard</h5>
+            <h5 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faHistory} /></span>History</h5>
+            <h5 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faCalendar} /></span>Calendar</h5>
+            <h5 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faExpandAlt} /></span>Appointments</h5>
+            <h5 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faChartSimple} /></span>Statistics</h5>
+            <h5 class="general">Tools</h5>
+            <h5 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faPerson} /></span>Chat</h5>
+            <h5 style={{color:"grey"}} class="project"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faPhone} /></span>Support</h5>
         </div>
         <div >
-            <h6 style={{color:"grey"}} class="support"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faPhone} /></span>Settings</h6>
+            <h5 style={{color:"grey"}} class="setting"><span class="project_icon"><FontAwesomeIcon className='search_icon' icon={faGear} /></span>Settings</h5>
+        </div>
         </div>
     </div>
-    // </div>
   )
 }
 
