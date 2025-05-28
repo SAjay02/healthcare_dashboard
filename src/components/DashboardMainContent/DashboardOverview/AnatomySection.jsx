dd49760d69e3ea4ed85343658bb58350c72b9b46
SAjay02 <56510.ajay@gmail.com>
2025-05-28 10:41:11 +0530

    fix: healty content on dashboard

--- src/components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
+++ src/components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
@@ -1,9 +1,22 @@
 import anatomy from "../../../assests/Anatomy.png"
+import "../../../styles/Dashboard/Dashboard.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart,faGavel } from '@fortawesome/free-solid-svg-icons'
 const AnatomySection = () => {
   return (
-    <div className='anatomy_container'>
-        <img className='anatomy_image' src={anatomy}/>
-    </div>
+    <>
+      <div className='anatomy_container'>
+          <img className='anatomy_image' src={anatomy}/>
+      </div>
+      <div className="heart_container">
+        <FontAwesomeIcon style={{color:"red",marginRight:"5px"}} className='' icon={faHeart} />
+        <h6>Healthy Heart</h6>
+      </div>
+      <div className="leg_container">
+        <FontAwesomeIcon style={{color:"lightgreen",marginRight:"5px"}} className='' icon={faGavel} />
+        <h6>Healthy Leg</h6>
+      </div>
+      </>
   )
 }
 
